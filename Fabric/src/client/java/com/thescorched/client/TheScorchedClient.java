01d3efd6bda231b11aa3a6e52ca185566680ccc1
Developer <developer@trustcredit.app>
2026-05-24 23:29:38 +0530

    Create

---
+++ Fabric/src/client/java/com/thescorched/client/TheScorchedClient.java
@@ -0,0 +1,10 @@
+package com.thescorched.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheScorchedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
